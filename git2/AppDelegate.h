72f9c0b5b20bd6e47c31b9ec07f67f6f4ea89a04
zhaohaojie <1390135920@qq.com>
2015-12-24 14:28:11 +0800

    Initial Commit

---
+++ git2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  git2
+//
+//  Created by 赵浩杰 on 15/12/24.
+//  Copyright © 2015年 赵浩杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ git2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git2
+//
+//  Created by 赵浩杰 on 15/12/24.
+//  Copyright © 2015年 赵浩杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
